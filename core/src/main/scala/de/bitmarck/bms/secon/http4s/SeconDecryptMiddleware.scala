897d32e0914481c80bff1e6d875e534a88487b48
Pierre Kisters <pierrekisters@gmail.com>
2023-04-17 18:11:41 +0200

    ContextMiddleware

--- core/src/main/scala/de/bitmarck/bms/secon/http4s/SeconDecryptMiddleware.scala
+++ core/src/main/scala/de/bitmarck/bms/secon/http4s/SeconDecryptMiddleware.scala
@@ -3,22 +3,26 @@ package de.bitmarck.bms.secon.http4s
 import cats.data.OptionT
 import cats.effect.Async
 import de.bitmarck.bms.secon.fs2.{CertLookup, DecryptVerify, IdentityLookup, SignEncrypt}
-import org.http4s.HttpRoutes
+import org.http4s.{ContextRequest, ContextRoutes, HttpRoutes}
 
 class SeconDecryptMiddleware[F[_] : Async : DecryptVerify : SignEncrypt](
                                                                           identityLookup: IdentityLookup[F],
                                                                           certLookup: CertLookup[F]
                                                                         ) {
-  def apply(routes: HttpRoutes[F]): HttpRoutes[F] = HttpRoutes[F] { request =>
+  def context(routes: ContextRoutes[SeconMetadata, F]): HttpRoutes[F] = HttpRoutes[F] { request =>
     for {
       incomingSeconRequest <- OptionT.liftF(request.as[SeconMessage[F]])
       outgoingSeconRequest = incomingSeconRequest.decryptAndVerify(identityLookup, certLookup)
       requestMedia <- OptionT.liftF(SeconMessage.seconOctetStreamEncoder.toMedia(outgoingSeconRequest))
-      response <- routes(request.withMedia(requestMedia))
+      response <- routes(ContextRequest(outgoingSeconRequest.metadata, request.withMedia(requestMedia)))
       incomingSeconResponse = outgoingSeconRequest.withMedia(response)
       outgoingSeconResponse = incomingSeconResponse.signAndEncrypt(identityLookup, certLookup, request = false)
       responseMedia <- OptionT.liftF(incomingSeconRequest.encoder.toMedia(outgoingSeconResponse))
     } yield
       response.withMedia(responseMedia)
   }
+
+  def apply(routes: HttpRoutes[F]): HttpRoutes[F] = context(ContextRoutes { case ContextRequest(_, request) =>
+    routes(request)
+  })
 }
